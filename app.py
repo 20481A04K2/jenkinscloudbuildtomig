8459fc3b4438214ff7a9fe24bc3d020fbcba9417
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-06-30 15:13:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Hello from Cloud Run deployed via jenkins!"
+    return "✅ Hello from mig deployed via jenkins!"
 
 if __name__ == '__main__':
     # Cloud Run expects the app to listen on port 8080 and 0.0.0.0
